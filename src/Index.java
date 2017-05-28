2932f30062a84d5465c69b1f61d36d0e83df2560
Shuyang Liu <s3610584@student.rmit.edu.au>
2017-05-28 21:40:33 +1000

    last bug!!!!

--- src/Index.java
+++ src/Index.java
@@ -1,62 +1,69 @@
 
+import java.io.File;
 import java.util.Random;
 
+/**
+ * Created by codoge on 2017/5/24.
+ */
 public class Index {
-	public static int MAX_ID = 1764576;
-	public static int MAX_KEY = 11284;
-	public static int MAX_PAGE_ID = 89695;
-	BTree tree;
-	public static String INDEX_PATH = "../disc/index";
+    public static int MAX_ID = 1764576;
+    public static int MAX_KEY = 11284;
+    public static int MAX_PAGE_ID = 89695;
+    BTree tree;
+    public static String INDEX_PATH = "../disc/index";
 
-	public Index() {
-		tree = new BTree();
-	}
+    public Index() {
+        tree = new BTree();
+    }
 
-	public void buildIndex(String path) {
-		int size = HeapPage.size / Record.bytes();
-		for (int pageNum = 0; pageNum < MAX_PAGE_ID; pageNum++) {
-			HeapPage page = HeapPage.readFromFile(path + "/" + pageNum + ".page");
-			Record[] records = page.getRecords();
-			for (int i = 0; i < records.length; i++) {
-				if (records[i].getID() > MAX_ID)
-					break;
-				tree.put(records[i].getHourly_Counts(), pageNum * size + i);
-			}
-		}
-		System.out.println("size:  " + tree.size());
-		System.out.println("height:" + tree.height());
-		System.out.println(tree);
-		System.out.println();
+    public void buildIndex(String path) {
+        int size = HeapPage.size / Record.bytes();
+        File f = new File(path);
+        File[] pageList = f.listFiles();
+        for (File file : pageList) {
+            String pageName = file.getName();
+            HeapPage page = HeapPage.readFromFile(path + "/" + pageName);
+            Record[] records = page.getRecords();
+            for (int i = 0; i < records.length; i++) {
+                if (records[i].getID() > MAX_ID)
+                    break;
+                int pageNum = Integer.parseInt(pageName.substring(0, pageName.length() - 5));
+                tree.put(records[i].getHourly_Counts(), pageNum * size + i);
+            }
+        }
+        System.out.println("size:  " + tree.size());
+        System.out.println("height:" + tree.height());
+        System.out.println(tree);
+        System.out.println();
 
-		tree.store(INDEX_PATH);
+        tree.store("disc/index");
 
-	}
+    }
+    public BTree.BTreeStore rebuildTree(String path) {
+        BTree.BTreeStore bst = tree.getStoreTree(path);
+        return bst;
+    }
 
-	public BTree.BTreeStore rebuildTree(String path) {
-		BTree.BTreeStore bst = tree.getStoreTree(path);
-		return bst;
-	}
-
-	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		Index index = new Index();
-		index.buildIndex(HeapFile.PAGE_PATH);
-		BTree.BTreeStore bst = index.rebuildTree(INDEX_PATH);
-		long end = System.currentTimeMillis();
-		System.out.println("Building index last for " + (end - start) + "ms.");
-		Searcher searcher = new Searcher(bst);
-		Random rand = new Random();
-		start = System.currentTimeMillis();
-		for (int i = 0; i < 3; i++) {
-			int randomID = rand.nextInt(MAX_KEY) + 1;
-			System.out.println(randomID);
-			searcher.printSearch(randomID);
-		}
-		//searcher.printSearch(9);
-		//searcher.printSearch(4712);
-		//searcher.printSearch(3682);
-		end = System.currentTimeMillis();
-		System.out.println("Using index to search spending " + (end - start) / 3 + "ms for average.");
-	}
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Index index = new Index();
+        index.buildIndex(HeapFile.PAGE_PATH);
+        BTree.BTreeStore bst = index.rebuildTree(INDEX_PATH);
+        long end = System.currentTimeMillis();
+        System.out.println("Building index last for " + (end - start) + "ms.");
+        Searcher searcher = new Searcher(bst);
+        Random rand = new Random();
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 3; i++) {
+            int randomID = rand.nextInt(MAX_KEY) + 1;
+            System.out.println(randomID);
+            searcher.printSearch(randomID);
+        }
+//        searcher.printSearch(9);
+//        searcher.printSearch(4712);
+//        searcher.printSearch(3682);
+        end = System.currentTimeMillis();
+        System.out.println("Using index to search spending " + (end - start)/3 + "ms for average.");
+    }
 
 }
